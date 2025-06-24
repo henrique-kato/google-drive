2d1bbdaa0a46f927daccf9c2597648ae8719edfd
henrique-kato <henrique@softable.com.br>
2025-06-24 12:22:33 -0300

    feat: add Header component

---
+++ app/src/components/commons/Header.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import { Profile } from "./Profile";
+
+const HeaderStyle = styled.div`
+  padding: 1rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  border-bottom: 1px solid white;
+  color: white;
+`;
+
+function Header(){
+  return (
+    <HeaderStyle>
+      <div>SOFTABLE</div>
+      <Profile/>
+    </HeaderStyle>
+  )
+}
+
+export { Header }
---
+++ app/src/components/commons/Profile.tsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+const ProfilePic = styled.div`
+  border-radius: 100%;
+  width: 24px;
+  height: 24px;
+  background-color: aliceblue;
+`
+
+const ProfileStyle = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+function Profile(){
+  return (
+    <ProfileStyle>
+      <div>Henrique Kato</div>
+      <ProfilePic/>
+    </ProfileStyle>
+  )
+}
+
+export { Profile }
